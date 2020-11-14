d8791d7e13d65a8960ffa344ff061973574fc506
dianprsty <dprasmail@gmail.com>
2020-11-14 08:22:08 +0700

    Native2

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import Main from './src/index'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Main />
+      </View>
+    )
+  }
+}
